d8ca7f515c05d79ce3cc01df435b2f5b8b06b102
damienroelens <roelens_dam@hotmail.com>
2013-11-04 17:42:51 +0100

    Début application

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,13 @@
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\gen\com\phonegap\projet\R.java \
+ : C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\res\drawable\icon.png \
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\res\drawable-hdpi\splash.9.png \
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\res\drawable-ldpi\splash.9.png \
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\res\drawable-mdpi\splash.9.png \
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\res\drawable-xhdpi\splash.9.png \
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\res\values\strings.xml \
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\res\xml\config.xml \
+C:\Users\vdb040\Desktop\ProjetPfe\platforms\android\bin\AndroidManifest.xml \
